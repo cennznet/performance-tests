4de59b78fa6e99f50b7f0123c7c70f30b97ac810
jinshi <jin.shi@centrality.ai>
2019-01-14 16:55:59 +1300

    Update jenkinsfile to make the image name to be a variable. Update topup.js to support manual set start id

--- Jenkinsfile
+++ Jenkinsfile
@@ -14,8 +14,12 @@ pipeline {
   stages {
     stage('Build Docker Image') {
       steps {
+        script{
+            dockerImageName = 'perftest_substrate'
+        }
+        
         echo 'build image...'
-        sh 'docker build -t perftest_substrate .' 
+        sh 'docker build -t ${dockerImageName} .' 
 
         // echo 'clean useless images...'
         // sh 'set +e' // ignore error below
@@ -29,7 +33,14 @@ pipeline {
 
     stage('Run test') {
       steps {
-        sh 'docker run perftest_substrate --once --user=10 --ws=ws://3.1.51.215:9944'   
+        echo 'Topup test address...'
+        sh 'docker run ${dockerImageName} --topup -i 50 -e local -s 0 -c 10 --ws=ws://3.1.51.215:9944'
+
+        echo 'Run one-time test...'
+        sh 'docker run ${dockerImageName} --once --user=10 --ws=ws://3.1.51.215:9944'   
+
+        echo 'Run simnple load test...'
+        sh 'docker run ${dockerImageName} --user=10 -startuser=1 --pacingtime=1 --rampuprate=1 --stepuser=5 --stepholdtime=30 --finalholdtime=60 --ws=ws://3.1.51.215:9944'
       }
     }
   }
--- tools/topup.js
+++ tools/topup.js
@@ -5,6 +5,8 @@ require('../src/api/general')
 var interval = 0;
 var testEnv = '';
 var fromSeedLst = [];
+var topupCnt = 0;
+var startNum = 0;
 
 function getArgs()
 {
@@ -12,16 +14,18 @@ function getArgs()
     argv.ws ? wsIp = argv.ws : wsIp = 'ws://127.0.0.1:9944';
     argv.i ? interval = argv.i : interval = 50;    // time interval
     argv.e ? testEnv = argv.e : testEnv = 'local'   // test environment
+    argv.c ? topupCnt = argv.c : topupCnt = 10000   // total address count to topup, default is 10k
+    argv.s ? startNum = argv.s : startNum = 0;
 }
 
 // test code
-async function topup(fileName, startId = 0) {
+async function topup(fileName, startId = 0, endId = 10000) {
     // let fromSeedLst = ['Alice','Bob','Eve','Dave']                       // local machine
     // let fromSeedLst = ['Andrea','Brooke','Courtney','Drew','Emily','Frank'] // dev
 
     let addrLst = await loadAddrFile(__dirname + '/../data/' + fileName)
 
-    for ( let i = startId; i < addrLst.length; i++ )
+    for ( let i = startId; i < addrLst.length && i < endId ; i++ )
     {
         let seedId = i % fromSeedLst.length
         let seed = fromSeedLst[seedId]
@@ -57,15 +61,15 @@ async function topupAll()
         fromSeedLst = ['Alice','Bob','Eve','Dave']  
     }
 
-    await topup('address_from.csv')
-    await topup('address_to.csv')
-    process.exit(1)
+    await topup('address_from.csv', startNum, endId = startNum + topupCnt)
+    await topup('address_to.csv', startNum, endId = startNum + topupCnt)
+    process.exit()
 }
 
 module.exports = topupAll;
 // topupAll()
 
 /*  run cmd:
-    1. local:   node src/run --topup -i 100 -e dev
-    2. dev:     node src/run --topup -i 100 -e local --ws ws://3.1.51.215:9944
+    1. local:   node src/run --topup -i 100 -e dev -s 0 -c 1000
+    2. dev:     node src/run --topup -i 100 -e local -s 0 -c 1000 --ws ws://3.1.51.215:9944
 */
